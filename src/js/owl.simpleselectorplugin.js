8f49390f3f88012e082ff4f9322b0b5b2f9e8cc2
Blaize Kaye <blaize.kaye@gmail.com>
2018-03-17 23:23:00 +1300

    first commit POC

---
+++ src/js/owl.simpleselectorplugin.js
@@ -0,0 +1,30 @@
+/**
+ * Plugin Name
+ * @since 2.0.0
+ */
+;(function ( $, window, document, undefined ) {
+    SimpleSelectorPlugin = function(scope){
+	    var that = this,
+      this.owl = scope,
+		  this.removableClasses = [],
+      this.owl._options = $.extend({}, SimpleSelectorPlugin.Defaults, this.owl.options);
+
+			this.owl.$element.on('initialize.owl.carousel', function(event) {
+			  if(!that.owl.$element.hasClass(that.owl._options.baseClass)) {
+			    that.owl.$element.addClass(that.owl._options.baseClass);
+          that.removableClasses.push(that.owl._options.baseClass);
+        }
+      });
+    }
+
+    SimpleSelectorPlugin.Defaults = {
+        baseClass: 'owl-carousel'
+    }
+
+    SimpleSelectorPlugin.prototype.destroy = function(){
+      for(var i = 0; i < this.removableClasses.length; i++) {
+        this.owl.$element.removeClass(this.removableClasses[i]);
+      }
+    };
+    $.fn.owlCarousel.Constructor.Plugins['pluginName'] = SimpleSelectorPlugin;
+})( window.Zepto || window.jQuery, window,  document );
